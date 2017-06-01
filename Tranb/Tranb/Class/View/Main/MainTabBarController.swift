cc27b96e95bb32e99925649e30058926dc0c31c9
KlayThompson <913661647@qq.com>
2017-06-01 21:57:04 +0800

    使用字典创建子控制器

--- Tranb/Tranb/Class/View/Main/MainTabBarController.swift
+++ Tranb/Tranb/Class/View/Main/MainTabBarController.swift
@@ -14,6 +14,7 @@ class MainTabBarController: UITabBarController {
         super.viewDidLoad()
         view.backgroundColor = UIColor.cz_random()
         // Do any additional setup after loading the view.
+        setupChildViewController()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +22,56 @@ class MainTabBarController: UITabBarController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+extension MainTabBarController {
+    
+    ///设置子控制器
+    func setupChildViewController() {
+        
+        //创建一个存放字典的数组
+        let dicArray = [
+            ["className" : "HomeViewController", "title" : "首页", "imageName" : ""],
+            ["className" : "MessageViewController", "title" : "消息", "imageName" : ""],
+            ["className" : "DiscoveryViewController", "title" : "发现", "imageName" : ""],
+            ["className" : "ProfileViewController", "title" : "我", "imageName" : ""],
+        ]
+        
+        //创建一个存放viewcontroller的数组
+        var vcArray = [UIViewController]()
+        
+        //遍历dicArray
+        for dic in dicArray {
+            vcArray.append(viewController(dictionary: dic))
+        }
+        
+        viewControllers = vcArray
     }
-    */
-
+    
+    
+    /// 使用字典创建子控制器
+    ///
+    /// - Parameter dictionary: 关于viewcontroller等相关参数 name、imageName、className
+    /// - Returns: UIviewcontroller
+    func viewController(dictionary: [String: String]) -> UIViewController {
+        
+        
+        guard let className = dictionary["className"],
+            let imageName = dictionary["imageName"],
+            let title = dictionary["title"],
+        let clas = NSClassFromString(Bundle.main.namespace + "." + className) as? UIViewController.Type
+            else {
+        
+                return UIViewController()
+        }
+        
+        let viewConttroller = clas.init()
+        viewConttroller.title = title
+        
+        let navi = MainNavigationController(rootViewController: viewConttroller)
+        
+        return navi
+    }
+    
 }
