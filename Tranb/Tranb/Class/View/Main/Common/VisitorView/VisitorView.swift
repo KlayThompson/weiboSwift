e96f395be4961f2ee5cdac68227b002cf240a77c
KlayThompson <913661647@qq.com>
2017-06-04 23:16:50 +0800

    访客界面苹果原生布局约束

--- Tranb/Tranb/Class/View/Main/Common/VisitorView/VisitorView.swift
+++ Tranb/Tranb/Class/View/Main/Common/VisitorView/VisitorView.swift
@@ -19,6 +19,22 @@ class VisitorView: UIView {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    //创建界面元素
+    //icon 转圈的
+    lazy var iconImageView = UIImageView(image: UIImage(named: "visitordiscover_feed_image_smallicon"))
+    
+    //大的houseIcon
+    lazy var hosueIcon = UIImageView(image: UIImage(named: "visitordiscover_feed_image_house"))
+    
+    //提示文本
+    lazy var tipLabel:UILabel = UILabel.cz_label(withText: "关注一些人，回这里看看有什么惊喜对的对的，关注一些人，回这里看看有什么惊喜奥术大师", fontSize: 14, color: UIColor.darkGray)
+    
+    //登录
+    lazy var loginButton:UIButton = UIButton.cz_textButton("登录", fontSize: 14, normalColor: UIColor.darkGray, highlightedColor: UIColor.black, backgroundImageName: "common_button_white_disable")
+    
+    //注册按钮
+    lazy var signInButton:UIButton = UIButton.cz_textButton("注册", fontSize: 14, normalColor: UIColor.darkGray, highlightedColor: UIColor.black, backgroundImageName: "common_button_white_disable")
 
 }
 
@@ -28,6 +44,41 @@ extension VisitorView {
     func setupUI() {
         
         backgroundColor = UIColor.white
+        
+        addSubview(iconImageView)
+        addSubview(hosueIcon)
+        addSubview(tipLabel)
+        addSubview(loginButton)
+        addSubview(signInButton)
+        
+        //取消autoresizing
+        for view in self.subviews {
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        //添加约束
+        //icon
+        addConstraint(NSLayoutConstraint(item: iconImageView, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: iconImageView, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1.0, constant: -60))
+        
+        //house
+        addConstraint(NSLayoutConstraint(item: hosueIcon, attribute: .centerX, relatedBy: .equal, toItem: iconImageView, attribute: .centerX, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: hosueIcon, attribute: .centerY, relatedBy: .equal, toItem: iconImageView, attribute: .centerY, multiplier: 1.0, constant: 0))
+        
+        //提示label
+        addConstraint(NSLayoutConstraint(item: tipLabel, attribute: .centerX, relatedBy: .equal, toItem: iconImageView, attribute: .centerX, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: tipLabel, attribute: .top, relatedBy: .equal, toItem: iconImageView, attribute: .bottom, multiplier: 1.0, constant: 20))
+        addConstraint(NSLayoutConstraint(item: tipLabel, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 300))
+        
+        //登录按钮
+        addConstraint(NSLayoutConstraint(item: loginButton, attribute: .left, relatedBy: .equal, toItem: tipLabel, attribute: .left, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: loginButton, attribute: .top, relatedBy: .equal, toItem: tipLabel, attribute: .bottom, multiplier: 1.0, constant: 20))
+       addConstraint(NSLayoutConstraint(item: loginButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 110))
+       
+        //注册按钮
+        addConstraint(NSLayoutConstraint(item: signInButton, attribute: .right, relatedBy: .equal, toItem: tipLabel, attribute: .right, multiplier: 1.0, constant: -10))
+        addConstraint(NSLayoutConstraint(item: signInButton, attribute: .top, relatedBy: .equal, toItem: tipLabel, attribute: .bottom, multiplier: 1.0, constant: 20))
+        addConstraint(NSLayoutConstraint(item: signInButton, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: 110))
         
     }
 }
