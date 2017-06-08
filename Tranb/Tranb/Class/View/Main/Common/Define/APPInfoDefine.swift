b368371f582b6d6400b8ec736aa72accffd5ac02
Perfor <913661647@qq.com>
2017-06-08 15:02:57 +0800

    增加APPInfo定义文件

---
+++ Tranb/Tranb/Class/View/Main/Common/Define/APPInfoDefine.swift
@@ -0,0 +1,17 @@
+//
+//  APPInfoDefine.swift
+//  Tranb
+//
+//  Created by Kim on 2017/6/8.
+//  Copyright © 2017年 KlayThompson. All rights reserved.
+//
+
+import Foundation
+
+/// APP信息相关配置
+let AppKey = "2291651912"
+let AppSecret = "76796a3b892b5666b9d42c1c8bd859a8"
+let Redirect_uri = "http://www.baidu.com"
+
+
+		
