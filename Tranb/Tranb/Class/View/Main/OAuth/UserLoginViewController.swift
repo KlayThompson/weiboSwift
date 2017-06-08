90d00c328c57376c50634bafca357999736c7b74
Perfor <913661647@qq.com>
2017-06-08 15:18:29 +0800

    拼接登录授权字符串，和登录界面加载

--- Tranb/Tranb/Class/View/Main/OAuth/UserLoginViewController.swift
+++ Tranb/Tranb/Class/View/Main/OAuth/UserLoginViewController.swift
@@ -15,14 +15,32 @@ class UserLoginViewController: UIViewController {
     /// 使用webView来进行登录授权界面
     lazy var webView = WKWebView()
     
+    override func loadView() {
+        super.loadView()
+        
+        view = webView
+        //        view.backgroundColor = UIColor.white
+        title = "登录微博"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "关闭", style: .plain, target: self, action: #selector(backButtonPress))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view = webView
-//        view.backgroundColor = UIColor.white
-        title = "登录微博"
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "关闭", style: .plain, target: self, action: #selector(backButtonPress))
-
+        let urlString = "https://api.weibo.com/oauth2/authorize?client_id=\(AppKey)&redirect_uri=\(Redirect_uri)"
+        print(urlString)
+        
+        /// 因为url可能为空，所以使用guard守护一下
+        guard let url = URL(string: urlString) else {
+            print("URL为空，请检查----")
+            return;
+        }
+        
+        
+        let request = URLRequest(url: url)
+        
+        //jiazai
+        webView.load(request)
     }
 
     func backButtonPress() {
