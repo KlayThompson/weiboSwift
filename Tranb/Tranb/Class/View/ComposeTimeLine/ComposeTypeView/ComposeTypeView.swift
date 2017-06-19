7d7dc0ffb8cd1cd14c2d7919b5a9d86499973aa3
Perfor <913661647@qq.com>
2017-06-19 16:36:57 +0800

    返回按钮实现

--- Tranb/Tranb/Class/View/ComposeTimeLine/ComposeTypeView/ComposeTypeView.swift
+++ Tranb/Tranb/Class/View/ComposeTimeLine/ComposeTypeView/ComposeTypeView.swift
@@ -15,14 +15,19 @@ class ComposeTypeView: UIView {
     @IBOutlet weak var closeButton: UIButton!
     ///存放button的
     @IBOutlet weak var scrollView: UIScrollView!
-    
+    //箭头返回按钮
+    @IBOutlet weak var backButtton: UIButton!
+    //返回箭头按钮竖直居中位置
+    @IBOutlet weak var backButtonCenterXCons: NSLayoutConstraint!
+    //关闭按钮竖直居中位置
+    @IBOutlet weak var closeButtonCenterXCons: NSLayoutConstraint!
     /// 按钮数据数组
     let buttonsInfo = [["imageName": "tabbar_compose_idea", "title": "文字"],
                                ["imageName": "tabbar_compose_photo", "title": "照片/视频"],
                                ["imageName": "tabbar_compose_weibo", "title": "长微博"],
                                ["imageName": "tabbar_compose_lbs", "title": "签到"],
                                ["imageName": "tabbar_compose_review", "title": "点评"],
-                               ["imageName": "tabbar_compose_more", "title": "更多"],
+                               ["imageName": "tabbar_compose_more", "title": "更多","actionName" : "moreButtonPress"],
                                ["imageName": "tabbar_compose_friend", "title": "好友圈"],
                                ["imageName": "tabbar_compose_wbcamera", "title": "微博相机"],
                                ["imageName": "tabbar_compose_music", "title": "音乐"],
@@ -60,6 +65,42 @@ class ComposeTypeView: UIView {
     @IBAction func closeButtonPress(_ sender: Any) {
         
         removeFromSuperview()
+    }
+    
+    //点击更多按钮
+    func moreButtonPress() {
+        print("点击更多")
+        //滚动到第二页
+        scrollView.setContentOffset(CGPoint(x: scrollView.bounds.width, y: 0), animated: true)
+        
+        //设置底部按钮
+        backButtton.isHidden = false
+        
+        //设置偏移
+        let offSet = scrollView.bounds.width / 4
+        
+        closeButtonCenterXCons.constant += offSet
+        backButtonCenterXCons.constant -= offSet
+        UIView.animate(withDuration: 0.3) {
+            self.layoutIfNeeded()
+        }
+    }
+    
+    //返回箭头按钮点击
+    @IBAction func backButtonPress(_ sender: Any) {
+        //返回到第一页
+        scrollView.setContentOffset(CGPoint(x: 0, y: 0), animated: true)
+        //让两个按钮合并
+        closeButtonCenterXCons.constant = 0
+        backButtonCenterXCons.constant = 0
+        UIView.animate(withDuration: 0.3, animations: { 
+            self.layoutIfNeeded()
+            self.backButtton.alpha = 0
+        }) { (_) in
+            self.backButtton.isHidden = true
+            self.backButtton.alpha = 1
+        }
+        
     }
     
     
@@ -112,6 +153,11 @@ private extension ComposeTypeView {
             
             //创建button
             let button = CompsoeTypeButton.compsoeTypeButton(imageName: imageName, titleName: title)
+            
+            //添加点击事件
+            if let actionName = dic["actionName"] {
+                button.addTarget(self, action: Selector(actionName), for: .touchUpInside)
+            }
             
             view.addSubview(button)
         }
